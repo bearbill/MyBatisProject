9bde31fe5a8031e53a69daffd72f0e5ab3c2e177
xiongweijun <13382364623@189.cn>
2019-08-07 22:37:03 +0800

    Initial commit

---
+++ src/com/xiong/mybatis/Main/ProjectAsMain.java
@@ -0,0 +1,23 @@
+package com.xiong.mybatis.Main;
+
+
+import com.xiong.mybatis.entry.Person;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.Reader;
+
+public class ProjectAsMain {
+    public static void main(String[] args) throws Exception{
+
+        Reader reader = Resources.getResourceAsReader("mybatisconfig.xml");
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Person persion = sqlSession.selectOne("mapping.personMapper.selectOnePresonById",1);
+
+        System.out.println(persion);
+    }
+}
